847c670803e65f93c0aab7042f87e413909ff87a
recofka <recofka@gmail.com>
2020-08-03 21:56:28 +0200

    created Dashboard component and style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositories on github</Title>
+      <Form>
+        <input placeholder="Type here" />
+        <button type="submit">Search</button>
+      </Form>
+      <Repositories>
+        <a href="https://github.com/recofka/waste-point-web">
+          <img
+            src="https://avatars0.githubusercontent.com/u/22827350?s=460&u=85b1413e88d5a645a375885936e29810faba1a3d&v=4"
+            alt="Denise Recofka"
+          />
+          <div>
+            <strong>recofka / waste-point-web</strong>
+            <p>React project with typescript</p>
+          </div>
+          <FiChevronRight />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
