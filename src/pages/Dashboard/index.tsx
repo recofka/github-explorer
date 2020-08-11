8c6de8d001e7f3822042298083b4ff3da5198b4e
recofka <recofka@gmail.com>
2020-08-11 17:06:25 +0200

    routes to repositories

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories, Error } from './styles';
@@ -75,9 +76,9 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a
+          <Link
             key={repository.full_name}
-            href={`https://github.com/${repository.full_name}`}
+            to={`/repositories/${repository.full_name}`}
           >
             <img
               src={repository.owner.avatar_url}
@@ -88,7 +89,7 @@ const Dashboard: React.FC = () => {
               <p>{repository.description}</p>
             </div>
             <FiChevronRight />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
